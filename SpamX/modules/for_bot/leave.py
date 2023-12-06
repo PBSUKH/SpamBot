ec17f263563e094b7d2c6025928987fdee52d341
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2023-12-06 20:43:38 +0530

    Update leave.py

--- SpamX/modules/for_bot/leave.py
+++ SpamX/modules/for_bot/leave.py
@@ -23,7 +23,7 @@ async def leave(SpamX: Client, e: Message):
     if len(e.text) > 7:
         chat = rizoel[0]
         try:
-           if chat in [-1001321613309, 1321613309, "@DNHxHELL"]:
+           if chat in [-1002056907061, -1002056907061, "THE_DRAMA_CLUB_01"]:
               return
            await SpamX.leave_chat(chat)
            await e.reply_text("**Left Successfully ✅ **")
@@ -34,7 +34,7 @@ async def leave(SpamX: Client, e: Message):
         ok = e.from_user.id
         if int(chat) == int(ok):
             return await e.reply_text(f"Usage: {handler}leave <chat username or id> or {handler}leave (type in Group for Direct leave)")
-        if int(chat) == -1001321613309:
+        if int(chat) == -1002056907061:
               return
         try:
            await SpamX.leave_chat(chat)
